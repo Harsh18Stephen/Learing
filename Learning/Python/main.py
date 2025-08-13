ea4eb5538b8011454a85e8a78e88125e7543ef86
Harsh <harsh.stephen189@gmail.com>
2025-08-13 11:46:20 +0530

    omg

---
+++ Learning/Python/main.py
@@ -0,0 +1,60 @@
+""" x= input("Enter the full name : ")
+
+print(len(x))
+print(x)
+
+print(str.find(x, "b"))
+ """
+
+""" for length in range(len(x)):
+    print(x[length]) """
+
+
+
+
+""" 
+x= int(input("Enter the number : "))
+
+if x%2==0:
+    print("Even")
+else:
+    print("Odd") """
+
+
+
+
+""" x = int(input("Enter the number : "))
+y = int(input("Enter the number : "))
+
+if x>y:
+    print(x, "is greater than", y)
+else:
+    print(y, "is greater than", x) """
+    
+
+
+
+""" x = int(input("Enter the number : "))
+
+if x%7 == 0 :
+   print("Divisible by 7")
+else:
+   print("Not divisible by 7")
+ """ 
+ 
+ 
+""" x = int(input("Enter the marks : "))
+
+if x>= 90:
+    print("A grade")
+elif x>=80 and x<90:
+    print("B grade")
+elif x>=70 and x<80:
+    print("C grade")
+elif x>=60 and x<70:
+    print("D grade")
+else:
+    print("Fail ") """ 
+    
+# tuples and lists and dictionaries and sets 
+
