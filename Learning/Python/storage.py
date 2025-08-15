f07372730b617264c198e92d92dabadb8267d445
Harsh <harsh.stephen189@gmail.com>
2025-08-15 16:04:49 +0530

    list and tuples

--- Learning/Python/storage.py
+++ Learning/Python/storage.py
@@ -1,2 +1,28 @@
 #tuples
 
+
+""" movie1 = input("Enter the first movie : ")
+movie2 = input("Enter the second movie : ")
+movie3 = input("Enter the third movie : ")
+movies = [movie1, movie2, movie3]
+movies.sort()
+print(movies) """
+
+
+
+""" 
+numbers = [1, 3, 3, 2, 1]
+
+if numbers == numbers[::-1]:
+    print("The list is symmetric")
+else:
+    print("The list is not symmetric")
+ """
+    
+    
+    
+""" my_list = ("C", "D", "A", "A", "B", "B", "A")
+print(my_list.count("A"))
+
+my_list.sort()
+print(my_list) """
