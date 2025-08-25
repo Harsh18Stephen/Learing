f55adb65ad9c46acca829dc31df0df56aae43eaf
Harsh <harsh.stephen189@gmail.com>
2025-08-25 15:12:54 +0530

    refresh

---
+++ Learning/Python/loops.py
@@ -0,0 +1,6 @@
+x = int(input("Enter a number to write table: "))
+i = int(input("Enter the range: "))
+
+while i < 0 :
+    print(x*i)
+    i -= i
--- Learning/Python/storage2.py
+++ Learning/Python/storage2.py
@@ -1,2 +1,32 @@
 # dictionary and sets
 
+""" my_Dict = {}
+
+
+my_Dict.update({"table" : "chair"})
+my_Dict.update({"cat" : "animal"})
+
+print(my_Dict)
+ """
+"""  
+subjects = {"python", "java", "c++","python", "javascript", "java", "python", "java", "c++", "c" }
+
+print(len(subjects)) """
+
+""" marks = {}
+
+marks.update({"python": input("Enter your marks in python: ")})
+marks.update({"java": input("Enter your marks in java : ")})
+marks.update({"c++": input("Enter your marks in c++: ")})
+
+print(marks) """
+
+
+numbers = {1, 2, 9.0}
+
+
+print(type(numbers))
+print(numbers)
+print(type(numbers))
+print(type(numbers))
+print(type(numbers))
